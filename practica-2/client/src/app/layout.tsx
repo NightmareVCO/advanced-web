124e5004dd5163a94560855d0a6982772f860844
NightmareVCO <vladimircuriel@outlook.com>
2025-02-05 23:20:37 -0400

    refact(client-p2): format code for consistency and readability across multiple files

--- practica-2/client/src/app/layout.tsx
+++ practica-2/client/src/app/layout.tsx
@@ -1,25 +1,25 @@
-import type { Metadata } from "next";
-import Providers from "./providers";
+import type { Metadata } from 'next';
 import { Onest } from 'next/font/google';
-import "./globals.css";
+import Providers from './providers';
+import './globals.css';
 
 export const metadata: Metadata = {
-  title: "Mockify",
-  description: "Mockify is an api's mockup tool for developers.",
+	title: 'Mockify',
+	description: "Mockify is an api's mockup tool for developers.",
 };
 
 const onest = Onest({ subsets: ['latin'] });
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="es" className="dark">
-        <body className={`antialiased ${onest.className}`}>
-            <Providers>{children}</Providers>
-        </body>
-    </html>
-  );
+	return (
+		<html lang="es" className="dark">
+			<body className={`antialiased ${onest.className}`}>
+				<Providers>{children}</Providers>
+			</body>
+		</html>
+	);
 }
