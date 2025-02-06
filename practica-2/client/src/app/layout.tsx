3af06ac551efaf9d163f58603615d4d532925117
Steven <smmr0001@ce.pucmm.edu.do>
2025-02-05 22:48:48 -0400

    feat(client-p2): [WIP] add nextjs client

---
+++ practica-2/client/src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import Providers from "./providers";
+import { Onest } from 'next/font/google';
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Mockify",
+  description: "Mockify is an api's mockup tool for developers.",
+};
+
+const onest = Onest({ subsets: ['latin'] });
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="es" className="dark">
+        <body className={`antialiased ${onest.className}`}>
+            <Providers>{children}</Providers>
+        </body>
+    </html>
+  );
+}
