9d28251acc0f9b7f48edf4b32283dd574ae9ffdd
NightmareVCO <vladimircuriel@outlook.com>
2025-02-10 16:29:38 -0400

    feat(exception-p2): add ForbiddenException and handle BadRequestException in ControllerExceptionHandler

---
+++ practica-2/src/main/java/com/icc/web/exception/ForbiddenException.java
@@ -0,0 +1,11 @@
+package com.icc.web.exception;
+
+public class ForbiddenException extends RuntimeException {
+
+  private static final long serialVersionUID = 1L;
+
+  public ForbiddenException(String message) {
+    super(message);
+  }
+
+}
