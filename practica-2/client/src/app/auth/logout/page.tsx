16c84581cac523a437407c5db2e54dd0c0a0969f
NightmareVCO <vladimircuriel@outlook.com>
2025-02-14 02:00:10 -0400

    feat(auth-p2): rename login component for clarity; enhance logout functionality with loading spinner; improve login form submission handling

--- practica-2/client/src/app/auth/logout/page.tsx
+++ practica-2/client/src/app/auth/logout/page.tsx
@@ -1,5 +1,20 @@
-import { logout } from '@lib/actions/logIn.action';
+'use client';
 
-export default async function LogOut() {
-	await logout();
+import { Spinner } from '@heroui/react';
+import { logout } from '@lib/actions/logIn.action';
+import { useEffect } from 'react';
+
+export default function LogOut() {
+	useEffect(() => {
+		const performLogout = async () => {
+			await logout();
+		};
+		performLogout();
+	}, []);
+
+	return (
+		<div className="flex justify-center items-center h-screen">
+			<Spinner color="primary" label="Logging Out" />
+		</div>
+	);
 }
