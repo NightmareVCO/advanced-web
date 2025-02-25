c389672bdd74b31895ff8e59591042a61503ced5
NightmareVCO <vladimircuriel@outlook.com>
2025-02-25 11:53:58 -0400

    feat(practica-3): refactor Docker setup and environment variable handling for server configuration

--- practica-3/client/src/lib/constants/constants.ts
+++ practica-3/client/src/lib/constants/constants.ts
@@ -1,2 +1,2 @@
 export const TEST_SERVER = 'localhost:8080';
-export const SERVER = 'app-prod:8080';
+export const SERVER = process.env.SERVER_PORT ? `server:${process.env.SERVER_PORT}` : 'server:8080'
