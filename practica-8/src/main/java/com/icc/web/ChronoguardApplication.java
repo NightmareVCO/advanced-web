104e1f54faa2db019d1c3def686c8001e63d1c0b
NightmareVCO <vladimircuriel@outlook.com>
2025-04-03 18:12:47 -0400

    feat(main-p8): initialize Chrono Guard application with basic configuration and landing page

---
+++ practica-8/src/main/java/com/icc/web/ChronoguardApplication.java
@@ -0,0 +1,13 @@
+package com.icc.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChronoguardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChronoguardApplication.class, args);
+	}
+
+}
---
+++ practica-8/src/main/java/com/icc/web/views/LandingView.java
@@ -0,0 +1,20 @@
+package com.icc.web.views;
+
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.html.Paragraph;
+
+@Route("")
+@PageTitle("Home | Chrono Guard")
+public class LandingView extends VerticalLayout {
+    public LandingView() {
+        setSizeFull();
+        add(new H1("Welcome to Chrono Guard!"));
+        add(new Paragraph("This is the landing page of the application."));
+        add(new Button("Get Started", e -> Notification.show("Button clicked!")));
+    }
+}
