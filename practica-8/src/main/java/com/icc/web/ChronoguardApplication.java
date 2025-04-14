1dd3e89c4a38116a24b61a39125e04181819c7bb
NightmareVCO <vladimircuriel@outlook.com>
2025-04-14 04:45:32 -0400

    feat(main-p8):  enable scheduling

--- practica-8/src/main/java/com/icc/web/ChronoguardApplication.java
+++ practica-8/src/main/java/com/icc/web/ChronoguardApplication.java
@@ -1,13 +1,17 @@
 package com.icc.web;
 
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-public class ChronoguardApplication {
+@EnableScheduling
+@Theme(value = "chornoguard")
+public class ChronoguardApplication implements AppShellConfigurator {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ChronoguardApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ChronoguardApplication.class, args);
+    }
 }
