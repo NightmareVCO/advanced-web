10e86b264cec9baeed0208c87d65bab6590b11ec
NightmareVCO <vladimircuriel@outlook.com>
2025-04-18 03:27:13 -0400

    feat(backgrounds-pf): add GridBackground and MiniGridBackground components with gradient styles

---
+++ proyecto-final/clientservice/src/components/backgrounds/GridBackground.tsx
@@ -0,0 +1,5 @@
+export default function GridBackground() {
+	return (
+		<div className="fixed inset-0 -z-10 h-full w-full bg-[#fbfbfb] bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]" />
+	);
+}
