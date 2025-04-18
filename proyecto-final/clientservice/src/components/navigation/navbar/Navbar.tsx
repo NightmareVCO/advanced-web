4a83b48e6f0166f9703a29008abee8f452e0c28c
NightmareVCO <vladimircuriel@outlook.com>
2025-04-18 03:23:51 -0400

    feat(navigation-pf): add Navbar & footer component and constants for navigation links

--- proyecto-final/clientservice/src/components/navigation/navbar/Navbar.tsx
+++ proyecto-final/clientservice/src/components/navigation/navbar/Navbar.tsx
@@ -1,7 +1,9 @@
 'use client';
 
+import BookHiveBranIcon from '@components/icons/BrandIcon';
 import {
 	Avatar,
+	Button,
 	Dropdown,
 	DropdownItem,
 	DropdownMenu,
@@ -13,86 +15,79 @@ import {
 	NavbarContent,
 	NavbarItem,
 } from '@heroui/react';
+import { type NavbarLink, NavbarLinks } from '@lib/constants/navbar.constants';
 
-export const AcmeLogo = () => {
-	return (
-		<svg fill="none" height="36" viewBox="0 0 32 32" width="36">
-			<title>Acme Logo</title>
-			<path
-				clipRule="evenodd"
-				d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
-				fill="currentColor"
-				fillRule="evenodd"
-			/>
-		</svg>
-	);
+// export const SearchIcon = ({
+// 	size = 24,
+// 	strokeWidth = 1.5,
+// 	width,
+// 	height,
+// 	...props
+// }) => {
+// 	return (
+// 		<svg
+// 			aria-hidden="true"
+// 			fill="none"
+// 			focusable="false"
+// 			height={height || size}
+// 			role="presentation"
+// 			viewBox="0 0 24 24"
+// 			width={width || size}
+// 			{...props}
+// 		>
+// 			<path
+// 				d="M11.5 21C16.7467 21 21 16.7467 21 11.5C21 6.25329 16.7467 2 11.5 2C6.25329 2 2 6.25329 2 11.5C2 16.7467 6.25329 21 11.5 21Z"
+// 				stroke="currentColor"
+// 				strokeLinecap="round"
+// 				strokeLinejoin="round"
+// 				strokeWidth={strokeWidth}
+// 			/>
+// 			<path
+// 				d="M22 22L20 20"
+// 				stroke="currentColor"
+// 				strokeLinecap="round"
+// 				strokeLinejoin="round"
+// 				strokeWidth={strokeWidth}
+// 			/>
+// 		</svg>
+// 	);
+// };
+
+type NavbarProps = {
+	navbarItems?: NavbarLink[];
 };
 
-export const SearchIcon = ({
-	size = 24,
-	strokeWidth = 1.5,
-	width,
-	height,
-	...props
-}) => {
-	return (
-		<svg
-			aria-hidden="true"
-			fill="none"
-			focusable="false"
-			height={height || size}
-			role="presentation"
-			viewBox="0 0 24 24"
-			width={width || size}
-			{...props}
-		>
-			<path
-				d="M11.5 21C16.7467 21 21 16.7467 21 11.5C21 6.25329 16.7467 2 11.5 2C6.25329 2 2 6.25329 2 11.5C2 16.7467 6.25329 21 11.5 21Z"
-				stroke="currentColor"
-				strokeLinecap="round"
-				strokeLinejoin="round"
-				strokeWidth={strokeWidth}
-			/>
-			<path
-				d="M22 22L20 20"
-				stroke="currentColor"
-				strokeLinecap="round"
-				strokeLinejoin="round"
-				strokeWidth={strokeWidth}
-			/>
-		</svg>
-	);
-};
-
-export default function Navbar() {
+export default function Navbar({ navbarItems }: NavbarProps) {
 	return (
 		<HeroNavbar isBordered>
 			<NavbarContent justify="start">
-				<NavbarBrand className="mr-4">
-					<AcmeLogo />
-					<p className="hidden sm:block font-bold text-inherit">ACME</p>
+				<NavbarBrand>
+					<BookHiveBranIcon width={80} height={100} />
 				</NavbarBrand>
-				<NavbarContent className="hidden sm:flex gap-3">
-					<NavbarItem>
-						<Link color="foreground" href="#">
-							Features
-						</Link>
-					</NavbarItem>
-					<NavbarItem isActive>
-						<Link aria-current="page" color="secondary" href="#">
-							Customers
-						</Link>
-					</NavbarItem>
-					<NavbarItem>
-						<Link color="foreground" href="#">
-							Integrations
-						</Link>
-					</NavbarItem>
-				</NavbarContent>
 			</NavbarContent>
 
-			<NavbarContent as="div" className="items-center" justify="end">
-				<Input
+			<NavbarContent className="hidden sm:flex" justify="center">
+				{navbarItems?.map((item: NavbarLink) => (
+					<NavbarItem key={item.label}>
+						<Link href={item.href} className="transition hover:scale-110">
+							{item.icon}
+							{item.label}
+						</Link>
+					</NavbarItem>
+				))}
+			</NavbarContent>
+
+			<NavbarContent as="div" justify="end">
+				<Button
+					as={Link}
+					href={NavbarLinks.LOGIN}
+					color="primary"
+					className="text-white"
+				>
+					LogIn
+				</Button>
+
+				{/* <Input
 					classNames={{
 						base: 'max-w-full sm:max-w-[10rem] h-10',
 						mainWrapper: 'h-full',
@@ -118,21 +113,16 @@ export default function Navbar() {
 						/>
 					</DropdownTrigger>
 					<DropdownMenu aria-label="Profile Actions" variant="flat">
-						<DropdownItem key="profile" className="h-14 gap-2">
+						<DropdownItem key="profile" className="gap-2 h-14">
 							<p className="font-semibold">Signed in as</p>
 							<p className="font-semibold">zoey@example.com</p>
 						</DropdownItem>
 						<DropdownItem key="settings">My Settings</DropdownItem>
-						<DropdownItem key="team_settings">Team Settings</DropdownItem>
-						<DropdownItem key="analytics">Analytics</DropdownItem>
-						<DropdownItem key="system">System</DropdownItem>
-						<DropdownItem key="configurations">Configurations</DropdownItem>
-						<DropdownItem key="help_and_feedback">Help & Feedback</DropdownItem>
 						<DropdownItem key="logout" color="danger">
 							Log Out
 						</DropdownItem>
 					</DropdownMenu>
-				</Dropdown>
+				</Dropdown> */}
 			</NavbarContent>
 		</HeroNavbar>
 	);
