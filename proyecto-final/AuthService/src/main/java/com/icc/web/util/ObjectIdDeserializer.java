bf6f65cc4046120e6e75ce126267051183c775dc
NightmareVCO <vladimircuriel@outlook.com>
2025-04-20 01:40:28 -0400

    refactor(authservice-pf): improve code formatting and remove unnecessary line breaks in deserializers and tests

--- proyecto-final/AuthService/src/main/java/com/icc/web/util/ObjectIdDeserializer.java
+++ proyecto-final/AuthService/src/main/java/com/icc/web/util/ObjectIdDeserializer.java
@@ -1,15 +1,17 @@
 package com.icc.web.util;
 
-import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
-import org.bson.types.ObjectId;
+import com.fasterxml.jackson.databind.JsonDeserializer;
 import java.io.IOException;
+import org.bson.types.ObjectId;
 
 public class ObjectIdDeserializer extends JsonDeserializer<ObjectId> {
 
     @Override
-    public ObjectId deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
+    public ObjectId deserialize(
+            JsonParser jsonParser, DeserializationContext deserializationContext)
+            throws IOException {
         return new ObjectId(jsonParser.getText());
     }
 }
--- proyecto-final/AuthService/src/main/java/com/icc/web/util/ObjectIdSerializer.java
+++ proyecto-final/AuthService/src/main/java/com/icc/web/util/ObjectIdSerializer.java
@@ -3,13 +3,14 @@ package com.icc.web.util;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
-import org.bson.types.ObjectId;
-
 import java.io.IOException;
+import org.bson.types.ObjectId;
 
 public class ObjectIdSerializer extends JsonSerializer<ObjectId> {
     @Override
-    public void serialize(ObjectId objectId, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+    public void serialize(
+            ObjectId objectId, JsonGenerator jsonGenerator, SerializerProvider serializerProvider)
+            throws IOException {
         jsonGenerator.writeString(objectId.toHexString());
     }
 }
