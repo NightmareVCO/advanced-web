e8e4c9d646674ceabaad73b18c4899ae2f1262c3
NightmareVCO <vladimircuriel@outlook.com>
2025-04-21 02:34:45 -0400

    feat(authservice-pf): refactor login logic in LoginController to use UserInfoService for authentication

--- proyecto-final/AuthService/src/main/java/com/icc/web/controller/LoginController.java
+++ proyecto-final/AuthService/src/main/java/com/icc/web/controller/LoginController.java
@@ -1,46 +1,50 @@
 package com.icc.web.controller;
 
+import com.icc.web.annotation.GatewayValidation;
 import com.icc.web.dto.AuthResponseDTO;
 import com.icc.web.dto.LoginDTO;
-import com.icc.web.exception.ResourceNotFoundException;
 import com.icc.web.exception.UnauthorizedException;
+import com.icc.web.model.UserInfo;
 import com.icc.web.service.JwtService;
+import com.icc.web.service.UserInfoService;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/auth/login/")
+@GatewayValidation
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/auth/login/")
 public class LoginController {
 
-    private final AuthenticationManager authenticationManager;
+    private final UserInfoService userInfoService;
     private final JwtService jwtService;
 
     @PostMapping
     public ResponseEntity<AuthResponseDTO> login(@RequestBody LoginDTO loginDTO) {
-        String userName = loginDTO.getUsername();
-        String password = loginDTO.getPassword();
-
-        Authentication authentication;
-        try {
-            authentication =
-                    authenticationManager.authenticate(
-                            new UsernamePasswordAuthenticationToken(userName, password));
-        } catch (AuthenticationException e) {
-            throw new UnauthorizedException("Invalid username or password");
+        String username = loginDTO.getUsername();
+        Optional<UserInfo> optUser = userInfoService.getUserByUsername(username);
+        if (optUser.isEmpty()) {
+            throw new UnauthorizedException("Invalid credentials");
         }
 
-        if (!authentication.isAuthenticated()) {
-            throw new ResourceNotFoundException("User not found");
+        UserInfo user = optUser.get();
+        if (!user.isActive()) {
+            throw new UnauthorizedException("User is not active");
         }
 
-        String token = jwtService.generateToken(authentication.getName()).getToken();
+        String storedPassword = user.getPassword();
+        String receivedPassword = loginDTO.getPassword();
+
+        boolean passwordMatches = userInfoService.checkPassword(receivedPassword, storedPassword);
+
+        if (!passwordMatches) {
+            throw new UnauthorizedException("Invalid credentials");
+        }
+
+        String token = jwtService.generateToken(username).getToken();
         AuthResponseDTO authResponseDTO = new AuthResponseDTO(token);
 
         return new ResponseEntity<>(authResponseDTO, HttpStatus.OK);
