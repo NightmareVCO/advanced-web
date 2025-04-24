52391049ba15a2f332d04e26075121b0285e22b6
NightmareVCO <vladimircuriel@outlook.com>
2025-04-23 20:16:01 -0400

    feat(catalogservice-pf): implement ResourceNotFoundException and enhance error handling in ControllerExceptionHandler

--- proyecto-final/CatalogService/src/main/java/com/icc/web/exception/ControllerExceptionHandler.java
+++ proyecto-final/CatalogService/src/main/java/com/icc/web/exception/ControllerExceptionHandler.java
@@ -10,16 +10,16 @@ import org.springframework.web.context.request.WebRequest;
 @RestControllerAdvice
 public class ControllerExceptionHandler {
 
-    // @ExceptionHandler(ResourceNotFoundException.class)
-    // @ResponseStatus(value = HttpStatus.NOT_FOUND)
-    // public ErrorMessage resourceNotFoundException(
-    //         ResourceNotFoundException ex, WebRequest request) {
-    //     return new ErrorMessage(
-    //             HttpStatus.NOT_FOUND.value(),
-    //             new Date(),
-    //             ex.getMessage(),
-    //             request.getDescription(false));
-    // }
+    @ExceptionHandler(ResourceNotFoundException.class)
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    public ErrorMessage resourceNotFoundException(
+            ResourceNotFoundException ex, WebRequest request) {
+        return new ErrorMessage(
+                HttpStatus.NOT_FOUND.value(),
+                new Date(),
+                ex.getMessage(),
+                request.getDescription(false));
+    }
 
     // @ExceptionHandler(ConflictException.class)
     // @ResponseStatus(value = HttpStatus.CONFLICT)
