b9d49cff02fd1230763cd31ec8958f9c5a0ba45c
NightmareVCO <vladimircuriel@outlook.com>
2025-04-23 20:08:03 -0400

    feat(clientservice-pf): update BooksSection and BookCategoryCards components for improved layout and navigation

--- proyecto-final/ClientService/src/components/sections/catalogPage/BooksSection.tsx
+++ proyecto-final/ClientService/src/components/sections/catalogPage/BooksSection.tsx
@@ -1,23 +1,27 @@
 'use client';
 
-import {
-	BreadcrumbItem,
-	Breadcrumbs,
-	Button,
-	Select,
-	SelectItem,
-	useDisclosure,
-} from '@heroui/react';
+import { Button, useDisclosure } from '@heroui/react';
 import { Icon } from '@iconify/react';
 import React from 'react';
 
-import SidebarDrawer from '@components/sidebarDrawer/SidebarDrawer';
-
+import ProductsGrid from '@/components/products/ProductsGrid';
 import FiltersWrapper from '@components/filterWrapper/FilterWrapper';
-import ProductsGrid from '@components/products/ProductsGird';
+import AuthorSearch from '@components/input/AuthorSearch';
+import TitleSearch from '@components/input/TitleSearch';
+import Pagination from '@components/pagination/Pagination';
+import SidebarDrawer from '@components/sidebarDrawer/SidebarDrawer';
 import ecommerceItems from '@lib/data/ecommerce.data';
+import type { BookResult } from '@lib/fetch/books.fetch';
+import { useSearchParams } from 'next/navigation';
 
-export default function BooksSection() {
+type BooksSectionProps = {
+	booksResult: BookResult;
+};
+
+export default function BooksSection({ booksResult }: BooksSectionProps) {
+	const searchParams = useSearchParams();
+	const pageParam = searchParams.get('page');
+	const page = pageParam ? Number.parseInt(pageParam, 10) : 1;
 	const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
 	return (
@@ -34,12 +38,20 @@ export default function BooksSection() {
 				</SidebarDrawer>
 				<div className="flex-col flex-1 w-full">
 					<header className="relative z-20 flex flex-col gap-2 px-4 pt-2 pb-3 rounded-medium bg-default-50 md:pt-3">
-						<div className="flex items-center gap-1 md:hidden md:gap-2">
-							<h2 className="font-medium text-large">Shoes</h2>
-							<span className="text-small text-default-400">(1240)</span>
+						<div className="flex items-center justify-between gap-1 md:hidden md:gap-2">
+							<div>
+								<h2 className="font-medium text-large">Books</h2>
+								<span className="text-small text-default-400">
+									({booksResult.total ?? 0})
+								</span>
+							</div>
+							<div className="flex flex-col items-center justify-center w-full gap-y-3">
+								<TitleSearch />
+								<AuthorSearch />
+							</div>
 						</div>
 						<div className="flex items-center justify-between gap-2 ">
-							<div className="flex flex-row gap-2">
+							<div className="flex flex-row w-full gap-2">
 								<Button
 									className="flex border-default-200 sm:hidden"
 									startContent={
@@ -55,41 +67,34 @@ export default function BooksSection() {
 								>
 									Filters
 								</Button>
-								<div className="items-center hidden gap-1 md:flex">
-									<h2 className="font-medium text-medium">Shoes</h2>
-									<span className="text-small text-default-400">(1240)</span>
+								<div className="items-center justify-between hidden w-full md:min-w-[750px] gap-1 md:flex">
+									<div className="flex items-center justify-center gap-1">
+										<h2 className="font-medium text-large">Books</h2>
+										<span className="text-small text-default-400">
+											({booksResult.total ?? 0})
+										</span>
+									</div>
+									<div className="flex items-center justify-end w-full gap-x-3">
+										<TitleSearch />
+										<AuthorSearch />
+									</div>
 								</div>
 							</div>
-							<Select
-								aria-label="Sort by"
-								classNames={{
-									base: 'items-center justify-end',
-									label:
-										'hidden lg:block text-tiny whitespace-nowrap md:text-small text-default-400',
-									mainWrapper: 'max-w-xs',
-								}}
-								defaultSelectedKeys={['most_popular']}
-								label="Sort by"
-								labelPlacement="outside-left"
-								placeholder="Select an option"
-								variant="bordered"
-							>
-								<SelectItem key="newest">Newest</SelectItem>
-								<SelectItem key="price_low_to_high">
-									Price: Low to High
-								</SelectItem>
-								<SelectItem key="price_high_to_low">
-									Price: High to Low
-								</SelectItem>
-								<SelectItem key="top_rated">Top Rated</SelectItem>
-								<SelectItem key="most_popular">Most Popular</SelectItem>
-							</Select>
 						</div>
 					</header>
 					<main className="w-full h-full px-1 mt-4 overflow-visible">
 						<div className="block border-dashed rounded-medium border-medium border-divider">
-							{/* Put your content here */}
-							<ProductsGrid className="grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4" />
+							<ProductsGrid
+								className="grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
+								products={booksResult.books ?? []}
+							/>
+						</div>
+						<div className="flex items-center justify-center w-full mt-5 mb-2">
+							<Pagination
+								total={booksResult.total ?? 0}
+								page={page}
+								pageSize={booksResult.pageSize ?? 10}
+							/>
 						</div>
 					</main>
 				</div>
--- proyecto-final/clientservice/src/components/cards/BookCategoryCards.tsx
+++ proyecto-final/clientservice/src/components/cards/BookCategoryCards.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import ScrollingBanner from '@components/scrollBanner/ScrollBanner';
-import { Card, CardHeader } from '@heroui/react';
+import { Card, CardHeader, Link } from '@heroui/react';
 import type { Category } from '@lib/data/category.data';
 import { motion } from 'framer-motion';
 import Image from 'next/image';
@@ -25,22 +25,24 @@ export default function BookCategoryCards({
 					whileHover={{ scale: 1.05 }}
 					whileTap={{ scale: 0.95 }}
 				>
-					<Card className="w-72 h-[300px] relative overflow-hidden">
-						<CardHeader className="absolute z-10 top-0 bg-primary flex-col !items-center">
-							<p className="font-bold uppercase text-tiny text-white/60">
-								{category.subtitle}
-							</p>
-							<h4 className="font-medium text-white text-large">
-								{category.title}
-							</h4>
-						</CardHeader>
-						<Image
-							src={category.image}
-							alt={`${category.title} category`}
-							fill
-							className="z-0 object-cover"
-						/>
-					</Card>
+					<Link href={category?.href}>
+						<Card className="w-72 h-[300px] relative overflow-hidden">
+							<CardHeader className="absolute z-10 top-0 bg-primary flex-col !items-center">
+								<p className="font-bold uppercase text-tiny text-white/60">
+									{category.subtitle}
+								</p>
+								<h4 className="font-medium text-white text-large">
+									{category.title}
+								</h4>
+							</CardHeader>
+							<Image
+								src={category.image}
+								alt={`${category.title} category`}
+								fill
+								className="z-0 object-cover"
+							/>
+						</Card>
+					</Link>
 				</motion.div>
 			))}
 		</ScrollingBanner>
