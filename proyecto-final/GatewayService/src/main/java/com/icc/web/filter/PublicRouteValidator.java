ead584098d91c5b26d1d36928f239a6f31bb4c86
Steven <smmr0001@ce.pucmm.edu.do>
2025-04-24 01:00:17 -0400

    feat(gatewayservice-pf): add email endpoints to public route validator

--- proyecto-final/GatewayService/src/main/java/com/icc/web/filter/PublicRouteValidator.java
+++ proyecto-final/GatewayService/src/main/java/com/icc/web/filter/PublicRouteValidator.java
@@ -8,7 +8,9 @@ import org.springframework.stereotype.Component;
 public class PublicRouteValidator {
 
     private static final List<String> PUBLIC_PATHS =
-            List.of("/api/v1/auth/", "/api/v1/books/", "/api/v1/books/search", "/api/v1/reviews/", "/api/v1/auth/login/", "/api/v1/auth/register/");
+            List.of("/api/v1/auth/", "/api/v1/books/", "/api/v1/books/search", "/api/v1/reviews/",
+                    "/api/v1/auth/login/", "/api/v1/auth/register/", "/api/v1/email/send-purchase",
+                    "/api/v1/email/send-registration");
 
     public boolean isPublicRoute(ServerHttpRequest request) {
         String path = request.getURI().getPath();
