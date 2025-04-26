19f81d9e0f3b50cf71b4f4ec7dec16ea153b4cdd
NightmareVCO <vladimircuriel@outlook.com>
2025-04-26 17:45:27 -0400

    feat(clientservice-pf): add ProductListItemNoCart and ProductsGridNoCart components for displaying products without a cart

---
+++ proyecto-final/ClientService/src/components/products/ProductsGridNoCart.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import { cn } from '@heroui/react';
+import React from 'react';
+
+import type { OrderItem } from '@/lib/fetch/orders.fetch';
+import ProductListItemNoCart from './ProductListItemNoCart';
+
+export type ProductGridProps = React.HTMLAttributes<HTMLDivElement> & {
+	itemClassName?: string;
+	products: OrderItem[];
+};
+
+const ProductsGridNoCart = React.forwardRef<HTMLDivElement, ProductGridProps>(
+	({ itemClassName, products, className, ...props }, ref) => {
+		return (
+			<div
+				ref={ref}
+				className={cn(
+					'grid w-full grid-cols-1 gap-0 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5',
+					className,
+				)}
+				{...props}
+			>
+				{products.map((product) => (
+					<ProductListItemNoCart
+						key={product.bookId}
+						removeWrapper
+						{...product}
+						className={cn('w-full snap-start', itemClassName)}
+					/>
+				))}
+				{products.length === 0 && (
+					<div className="flex items-center justify-center w-full h-full col-span-4">
+						<p className="text-default-500">No books found</p>
+					</div>
+				)}
+			</div>
+		);
+	},
+);
+
+ProductsGridNoCart.displayName = 'ProductsGridNoCart';
+
+export default ProductsGridNoCart;
