3ebaf2d4023d9c9ed5e3ef2b29d099d39cb231e8
NightmareVCO <vladimircuriel@outlook.com>
2025-04-27 07:03:43 -0400

    refactor(clientservice-pf): standardize formatting in useMemoizedCallback for improved readability

--- proyecto-final/ClientService/src/components/tables/booksTable/useMemoizedCallback.ts
+++ proyecto-final/ClientService/src/components/tables/booksTable/useMemoizedCallback.ts
@@ -1,24 +1,26 @@
-import { useMemo, useRef } from "react";
+import { useMemo, useRef } from 'react';
 
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
 type noop = (this: any, ...args: any[]) => any;
 
 type PickFunction<T extends noop> = (
-  this: ThisParameterType<T>,
-  ...args: Parameters<T>
+	this: ThisParameterType<T>,
+	...args: Parameters<T>
 ) => ReturnType<T>;
 
 export function useMemoizedCallback<T extends noop>(fn: T) {
-  const fnRef = useRef<T>(fn);
+	const fnRef = useRef<T>(fn);
 
-  fnRef.current = useMemo<T>(() => fn, [fn]);
+	fnRef.current = useMemo<T>(() => fn, [fn]);
 
-  const memoizedFn = useRef<PickFunction<T>>();
+	// @ts-ignore
+	const memoizedFn = useRef<PickFunction<T>>();
 
-  if (!memoizedFn.current) {
-    memoizedFn.current = function (this, ...args) {
-      return fnRef.current.apply(this, args);
-    };
-  }
+	if (!memoizedFn.current) {
+		memoizedFn.current = function (this, ...args) {
+			return fnRef.current.apply(this, args);
+		};
+	}
 
-  return memoizedFn.current as T;
+	return memoizedFn.current as T;
 }
