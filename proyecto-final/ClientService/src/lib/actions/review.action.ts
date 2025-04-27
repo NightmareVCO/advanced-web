5e7d95f5cca1a5bd4aef74f7b415bb09027890c9
NightmareVCO <vladimircuriel@outlook.com>
2025-04-27 07:04:27 -0400

    refactor(clientservice-pf): improve code formatting and consistency in review action

--- proyecto-final/ClientService/src/lib/actions/review.action.ts
+++ proyecto-final/ClientService/src/lib/actions/review.action.ts
@@ -8,7 +8,7 @@ const REVIEW_ENDPOINT = EndpointEnum.Reviews;
 const REVIEW_URL = `${API_URL}${REVIEW_ENDPOINT}`;
 
 const HEADERS = {
-  'Content-Type': 'application/json',
+	'Content-Type': 'application/json',
 };
 
 // {
@@ -20,76 +20,72 @@ const HEADERS = {
 // }
 
 type CreateReviewDTO = {
-  title: string;
-  bookId: string;
-  userId: string;
-  comment: string;
-  rating: number;
-}
+	title: string;
+	bookId: string;
+	userId: string;
+	comment: string;
+	rating: number;
+};
 
 const manageError = (error: unknown) => {
 	if (error instanceof Error) {
 		return {
 			errors: {
-        title: error.message,
-        comment: error.message,
+				title: error.message,
+				comment: error.message,
 			},
 		};
 	}
 	if (error === 'string') {
 		return {
 			errors: {
-        title: error,
-        comment: error,
+				title: error,
+				comment: error,
 			},
 		};
 	}
 	return {
 		errors: {
-      title: 'An unknown error occurred',
-      comment: 'An unknown error occurred',
+			title: 'An unknown error occurred',
+			comment: 'An unknown error occurred',
 		},
 	};
 };
 
-export const createReview = async (
-  prevState: unknown,
-  formData: FormData,
-) => {
-  try {
-    const data = Object.fromEntries(formData.entries());
+export const createReview = async (prevState: unknown, formData: FormData) => {
+	try {
+		const data = Object.fromEntries(formData.entries());
 
-    const createReviewDTO: CreateReviewDTO = {
-      title: data.title as string,
-      bookId: data.bookId as string,
-      userId: data.userId as string,
-      comment: data.comment as string,
-      rating: Number.parseInt(data.rating as string),
-    };
+		const createReviewDTO: CreateReviewDTO = {
+			title: data.title as string,
+			bookId: data.bookId as string,
+			userId: data.userId as string,
+			comment: data.comment as string,
+			rating: Number.parseInt(data.rating as string),
+		};
 
-    const response = await fetch(REVIEW_URL, {
-      method: 'POST',
-      headers: HEADERS,
-      cache: 'no-cache',
-      body: JSON.stringify(createReviewDTO),
-    });
+		const response = await fetch(REVIEW_URL, {
+			method: 'POST',
+			headers: HEADERS,
+			cache: 'no-cache',
+			body: JSON.stringify(createReviewDTO),
+		});
 
-    if (!response.ok) {
-      const errorResponse = await response.json();
-      console.error('Error response:', errorResponse);
-      return {
-        errors: {
-          title: errorResponse.message,
-          comment: errorResponse.message,
-        },
-      };
+		if (!response.ok) {
+			const errorResponse = await response.json();
+			console.error('Error response:', errorResponse);
+			return {
+				errors: {
+					title: errorResponse.message,
+					comment: errorResponse.message,
+				},
+			};
+		}
 
-    }
-
-    revalidatePath(`/catalog/books/${data.bookId}`);
-    return await response.json();
-  } catch (error: unknown) {
-    console.error('Error creating review:', error);
-    return manageError(error);
-  }
+		revalidatePath(`/catalog/books/${data.bookId}`);
+		return await response.json();
+	} catch (error: unknown) {
+		console.error('Error creating review:', error);
+		return manageError(error);
+	}
 };
